a8ca31cc7e0352884d7325fa8ebe5fd8d53d2711
chenjingyu <2458006366@qq.com>
2022-01-11 18:32:36 +0800

    Update：添加平面检测，放置方块功能

---
+++ HelloAR/Assets/ARPlacement.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARPlacement : MonoBehaviour
+{
+    public GameObject arObjectToSpawn;
+    public GameObject placementIndicator;
+    private GameObject spawnedObject;
+
+    private Pose placementPose;
+    private ARRaycastManager arRaycastManager;
+    private bool placementPoseIsValid = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceARObject();
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(
+                placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        arRaycastManager.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+
+    void PlaceARObject()
+    {
+        spawnedObject = Instantiate(arObjectToSpawn, placementPose.position, placementPose.rotation);
+    }
+}
